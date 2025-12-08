7dcd9beb78550cef05de6844887c8cbb98eb0707
Daniel Roberto <sdanirobert@outlook.com>
2025-12-08 11:20:00 +0000

    feat: add appointment date value object

---
+++ src/domain/value-objects/AppointmentDate.ts
@@ -0,0 +1,15 @@
+export class AppointmentDate {
+  private readonly value: Date
+
+  private constructor(value: Date) {
+    this.value = value
+  }
+
+  static create(value: Date): AppointmentDate {
+    return new AppointmentDate(value)
+  }
+
+  toDate(): Date {
+    return this.value
+  }
+}
