f097cf9a489f894db5f5a08daf344a63fb7a7f46
Daniel Roberto <sdanirobert@outlook.com>
2025-12-08 11:05:00 +0000

    feat: add identifier value object

---
+++ src/domain/value-objects/Identifier.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from "uuid"
+
+export class Identifier {
+  private readonly value: string
+
+  private constructor(value: string) {
+    this.value = value
+  }
+
+  static create(value?: string): Identifier {
+    return new Identifier(value ?? uuidv4())
+  }
+
+  toString(): string {
+    return this.value
+  }
+}
