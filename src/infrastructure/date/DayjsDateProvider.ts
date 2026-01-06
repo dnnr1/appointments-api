b7d26589b950c88c6e2b3778fefdc346dc6ecc82
Daniel Roberto <sdanirobert@outlook.com>
2026-01-06 09:20:00 +0000

    feat: add dayjs date provider

---
+++ src/infrastructure/date/DayjsDateProvider.ts
@@ -0,0 +1,24 @@
+import dayjs from "dayjs"
+import { DateProvider } from "../../application/interfaces/DateProvider"
+
+export class DayjsDateProvider implements DateProvider {
+  now(): Date {
+    return dayjs().toDate()
+  }
+
+  isBefore(a: Date, b: Date): boolean {
+    return dayjs(a).isBefore(b)
+  }
+
+  diffInHours(a: Date, b: Date): number {
+    return dayjs(a).diff(b, "hour")
+  }
+
+  startOfDay(date: Date): Date {
+    return dayjs(date).startOf("day").toDate()
+  }
+
+  endOfDay(date: Date): Date {
+    return dayjs(date).endOf("day").toDate()
+  }
+}
